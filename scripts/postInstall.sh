e0acd25aa52d53912ee6425a52033b3e5d8f8af7
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-07-22 12:16:45 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,13 +5,18 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 75s;
 
-docker-compose exec -T web bash -c "python3 manage.py sync-exercises"
-docker-compose exec -T web bash -c "python3 manage.py download-exercise-images"
-docker-compose exec -T web bash -c "python3 manage.py download-exercise-videos"
+echo "sync-exercises"
+docker-compose exec -T web bash -c "python3 manage.py sync-exercises" > /dev/null 2>&1
+echo "download-exercise-images"
+docker-compose exec -T web bash -c "python3 manage.py download-exercise-images" > /dev/null 2>&1
+echo "download-exercise-videos"
+docker-compose exec -T web bash -c "python3 manage.py download-exercise-videos" > /dev/null 2>&1
 
-docker-compose exec -T web bash -c "wger load-online-fixtures"
+echo "load-online-fixtures"
+docker-compose exec -T web bash -c "wger load-online-fixtures" > /dev/null 2>&1
 # afterwards:
-docker-compose exec -T web bash -c "python3 manage.py sync-ingredients"
+echo "sync-ingredients"
+docker-compose exec -T web bash -c "python3 manage.py sync-ingredients" > /dev/null 2>&1
 
 sleep 10s;
 
